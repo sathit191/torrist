0a9aca688f939f4d966d059b20eb031eaec5a5cd
tor <sathit.chainat@gmail.com>
2019-11-04 14:09:26 +0700

    test

---
+++ lib/screens/home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  //Explicit
+
+  //Method
+
+  Widget authenButton() {
+    return RaisedButton(
+      color: Colors.pinkAccent[700],
+      child: Text(
+        'Authentication',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      width: 150.0,
+      height: 150.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'Tor RIST',
+      style: TextStyle(
+        color: Colors.orange.shade900,
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, Colors.purple],
+              radius: 1.0,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showLogo(),
+                showAppName(),
+                authenButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
